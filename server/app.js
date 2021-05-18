024f422a4ac2a555658f268a5a2e0f06b7740308
Tyks23 <epic.raider99@gmail.com>
2021-05-18 17:35:02 +0300

    Add ServersideValidation

--- server/app.js
+++ server/app.js
@@ -4,22 +4,51 @@ const db = require("./db.js");
 const jwt = require("jsonwebtoken");
 const secret = "0832c1202da8d382318e329a7c133ea0";
 let app = express();
+const { body, validationResult } = require('express-validator');
+const { Query } = require("pg");
 
 app.use(cors());
 app.use(express.json());
-app.post("/register", async(req, res) => {
+/*app.post("/register", async(req, res) => {
     let body = req.body;
     console.log(body);
     await db.query(`Insert Into users(name, email, password_hash) Values ('${body.name}','${body.email}','${body.password}')`);
     res.send(body);
-});
+});*/
+
+
+  app.post(
+    '/register',  
+    body('email').isEmail(),
+    body('password').isLength({ min: 7 }),
+    async(req, res) => {
+       let body = req.body;
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      let query1 = await db.query(`Select name From users Where name = '${body.name}';`);
+      let query2 = await db.query(`Select email From users Where name = '${body.email}';`);
+    
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }else if(query1.rowCount > 0){
+        return res.status(400).json({ errors: "name already in use" });
+      }else if(query2.rowCount > 0){
+        return res.status(400).json({ errors: "email already in use" });
+      }
+      else{
+        await db.query(`Insert Into users(name, email, password_hash) Values ('${body.name}','${body.email}','${body.password}')`);
+        res.status(200).send();
+      }
+    });
+
+
 app.post("/login", async(req, res) => {
     let body = req.body;
     let query = await db.query(`Select * From users Where name = '${body.name}' AND password_hash = '${body.password}';`);
     if(query.rows.length !== 0){
         query = query.rows[0];
         let data = {
-            id : query["id"],
+            user_id : query["user_id"],
             name : query["name"]
         }
         res.send({token: jwt.sign(data, secret)});
@@ -29,14 +58,14 @@ app.post("/login", async(req, res) => {
     }
 });
 
-app.post("/sendAnswer", async(req, res) => {
+app.post("/sendanswer", async(req, res) => {
     let body = req.body;
     let token = req.header('Authorization').split(' ')[1];
 
     try{
         let user = jwt.verify(token, secret);
         console.log(user);
-        let query = await db.query(`Update participant Set answer = '{${Object.values(body)}}' Where part_id = '${user.id}' Returning *`);
+        let query = await db.query(`Update participant Set answer = '{${Object.values(body)}}' Where part_id = '${user.part_id}' Returning *`);
         if(query.rows.length !== 0){
             query = query.rows[0];
             res.status(200).send();
@@ -57,8 +86,8 @@ app.post("/enterquiz", async(req, res) => {
             query = query.rows[0];
             console.log(query)
             let data = {
-                id : query["part_id"],
-                name : query["part_name"]
+                part_id : query["part_id"],
+                part_name : query["part_name"]
             }
             res.send({token: jwt.sign(data, secret)});
         }else{
@@ -86,3 +115,35 @@ app.get("/", async(req, res) => {
     })
 });
 app.listen(3000);
+
+app.post("/creategroup", async(req, res) => {
+    let body = req.body;
+    let token = req.header('Authorization').split(' ')[1];
+    console.log(token);
+    try{
+        let user = jwt.verify(token, secret);
+        console.log(user);
+        let query = await db.query(`Insert Into group_instance(group_name, user_id) Values ('${body.group_name}','${user.user_id}') Returning *`);
+        if(query.rows.length !== 0){
+            query = query.rows[0];
+            res.status(200).send();
+        }
+    }catch(e){
+        console.log(e);
+        res.status(403).send();
+    }
+});
+
+app.get("/listgroup", async(req, res) => {
+    let token = req.header('Authorization').split(' ')[1];
+
+    try{
+        let user = jwt.verify(token, secret);
+        let query = await db.query(`Select * From group_instance Where group_instance.user_id = '${user.user_id}'`);
+        console.log(query);
+        res.status(200).send(query.rows);
+    }catch(e){
+        console.log(e);
+        res.status(403).send();
+    }
+});
